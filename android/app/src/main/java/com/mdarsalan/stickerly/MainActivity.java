cca8164cc660467a060d4276a4efd8f9de6c5002
MD ARSALAN <mdarsalan@vibein.com>
2026-08-31 23:20:49 +0530

    fix: register WhatsAppPlugin in MainActivity

--- android/app/src/main/java/com/mdarsalan/stickerly/MainActivity.java
+++ android/app/src/main/java/com/mdarsalan/stickerly/MainActivity.java
@@ -1,5 +1,12 @@
 package com.mdarsalan.stickerly;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(WhatsAppPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
