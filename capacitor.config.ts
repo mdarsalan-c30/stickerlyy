6a8faab7aaa0f90e66feba5b77bfad0d7cbca39c
MD ARSALAN <mdarsalan@vibein.com>
2026-08-31 20:13:04 +0530

    chore: setup Capacitor for Android APK generation

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mdarsalan.stickerly',
+  appName: 'stickerly',
+  webDir: 'out',
+  server: {
+    url: 'https://stickerlyy.vercel.app',
+    cleartext: true
+  }
+};
+
+export default config;
